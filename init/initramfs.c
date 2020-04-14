4358db25a9c0cc2b4d0a2e70b944301c0a000a20
Rodrigosolari <Rodrigo13_23@yahoo.es>
2020-04-13 22:39:31 -0500

    Update initramfs.c

--- init/initramfs.c
+++ init/initramfs.c
@@ -622,13 +622,14 @@ __setup("skip_initramfs", skip_initramfs_param);
 static int __init populate_rootfs(void)
 {
 	char *err;
-
+	//comented lines for Sailfish OS
+	/*
 	if (do_skip_initramfs) {
 		if (initrd_start)
 			free_initrd();
 		return default_rootfs();
 	}
-
+	*/
 	/* Load the built in initramfs */
 	err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
 	if (err)
